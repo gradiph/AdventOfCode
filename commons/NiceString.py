26cb465215084dab09b041baeae56bfa07f816ed
Gradiyanto Putera Husein <gradiyanto@vsi.co.id>
2021-12-25 17:53:34 +0700

    2015 Day 5 Part 2

--- commons/NiceString.py
+++ commons/NiceString.py
@@ -9,13 +9,20 @@ class NiceString:
 
     def validate(self):
         try:
-            self.validateRule1()
-            self.validateRule2()
-            self.validateRule3()
+            self.validateVowels()
+            self.validateTwiceInARow()
+            self.validateForbidden()
         except Exception as e:
             raise e
 
-    def validateRule1(self):
+    def validate2(self):
+        try:
+            self.validateTwoLetterTwice()
+            self.validateBetweenRepeat()
+        except Exception as e:
+            raise e
+
+    def validateVowels(self):
         nVowels = 0
 
         for char in self.word:
@@ -25,7 +32,7 @@ class NiceString:
         if nVowels < 3:
             raise Exception('ERROR Rule 1: It contains less than 3 vowels.')
 
-    def validateRule2(self):
+    def validateTwiceInARow(self):
         lastChar = ''
         isValidated = False
 
@@ -39,8 +46,61 @@ class NiceString:
             raise Exception('ERROR Rule 2: It doesn\'t contains at least one '
                             'letter that appears twice in a row.')
 
-    def validateRule3(self):
+    def validateForbidden(self):
         for forbiddenWord in self.forbiddens:
             if (forbiddenWord in self.word):
                 raise Exception('ERROR Rule 3: It contains the forbidden '
                                 'strings.')
+
+    def validateTwoLetterTwice(self):
+        firstChar = ''
+        secondChar = ''
+        letterPairs = list()
+
+        i = 0
+        for char in self.word:
+            if firstChar == '':
+                firstChar = char
+            else:
+                secondChar = char
+                letterPairs.append(firstChar + secondChar)
+                firstChar = secondChar
+            i += 1
+
+        isTwoLetterTwice = False
+        for pair in letterPairs:
+            searchIndex = letterPairs.index(pair)
+            searchingPairs = letterPairs.copy()
+            searchingPairs.pop(searchIndex)
+            try:
+                resultIndex = searchingPairs.index(pair)
+                # check for same value because of the first one is popped, so
+                # if it overlaps, then the index would be same.
+                if resultIndex != searchIndex:
+                    isTwoLetterTwice = True
+                    break
+            except Exception:
+                pass
+
+        if (not isTwoLetterTwice):
+            raise Exception('ERROR: It doesn\'t contain at least a pair of any'
+                            ' two letters that appears twice.')
+
+    def validateBetweenRepeat(self):
+        isBetweenRepeat = False
+        i = 0
+
+        for char in self.word:
+            try:
+                secondChar = self.word[i + 2]
+                if char == secondChar:
+                    isBetweenRepeat = True
+                    break
+                i += 1
+            except Exception:
+                break
+
+        if (not isBetweenRepeat):
+            raise Exception('ERROR: It doesn\'t contain at least one letter '
+                            'which repeats with exactly one letter between '
+                            'them.')
